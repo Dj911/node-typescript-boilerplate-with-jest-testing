4385249fb1f2f22b66b793dfcdfbe568586eb53f
Dj911 <dharmaraj.jadeja911@gmail.com>
2023-01-25 16:53:48 +0530

    refactoring test case

--- src/__tests__/module/users/routes.test.ts
+++ src/__tests__/module/users/routes.test.ts
@@ -3,39 +3,39 @@ import app from '../../../app'
 // jest.setTimeout(100000000)
 
 import request from 'supertest'
-import { expect, test } from 'vitest'
+import { expect, test, describe } from 'vitest'
 
-// describe('POST /user/signup ', () => {
-// })
-test('User signup: ', async () => {
-    const res = await 
-    request(app.app)
-            .post('/api/v1/user/signup')
-            .send({
-                "name": "DJ",
-                "email": "dharmaraj.jadeja911@gmail.com",
-                "password": "abcd123456",
-                "mobileNumber": "+917874594418"
-            })
-            .set('Accept', 'application/json')
-            // .end((err,res)=>{
-            //     if(err) console.log('ERROR: ',err)
-                
-            // })
-            if (res.statusCode === 201) {
-                expect(res.body).toMatchObject({
-                    "result": 1,
-                    "message": "Registration successfully",
-                    "data": {}
+describe('POST /user/signup ', () => {
+    test('User signup: ', async () => {
+        const res = await 
+        request(app.app)
+                .post('/api/v1/user/signup')
+                .send({
+                    "name": "DJ",
+                    "email": "dharmaraj.jadeja911@gmail.com",
+                    "password": "abcd123456",
+                    "mobileNumber": "+917874594418"
                 })
-            }
-    
-            if (res.statusCode === 500) {
-                expect(res.body).toMatchObject({
-                    "result": 0,
-                    "data": {}
-                })
-            }
+                .set('Accept', 'application/json')
+                // .end((err,res)=>{
+                //     if(err) console.log('ERROR: ',err)
+                    
+                // })
+                if (res.statusCode === 201) {
+                    expect(res.body).toMatchObject({
+                        "result": 1,
+                        "message": "Registration successfully",
+                        "data": {}
+                    })
+                }
+        
+                if (res.statusCode === 500) {
+                    expect(res.body).toMatchObject({
+                        "result": 0,
+                        "data": {}
+                    })
+                }
+    })
 })
 
 // afterAll(async () => {
